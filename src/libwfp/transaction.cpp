cd5bf8bc87210354eabfa38cc964d8d9c965ab9a
Odd Stranne <odd@mullvad.net>
2017-12-07 11:23:49 +0100

    Add class for simplifying working with transactions

---
+++ src/libwfp/transaction.cpp
@@ -0,0 +1,81 @@
+#include "stdafx.h"
+#include "transaction.h"
+#include <fwpmu.h>
+#include <stdexcept>
+
+Transaction::Transaction(std::shared_ptr<FilterEngine> engine, bool readWrite)
+	: m_engine(engine)
+	, m_aborted(false)
+{
+	auto status = FwpmTransactionBegin0((*engine).session(), (readWrite ? 0 : FWPM_TXN_READ_ONLY));
+
+	if (ERROR_SUCCESS != status)
+	{
+		//
+		// TODO: Add code that throws runtime error with message derived from "status"/GetLastError()
+		//
+		if (FWP_E_TXN_IN_PROGRESS == status)
+		{
+			throw std::runtime_error("Cannot begin new transaction, one is already in progress");
+		}
+
+		throw std::runtime_error("Cannot begin new transaction");
+	}
+}
+
+Transaction::~Transaction()
+{
+	if (m_aborted)
+	{
+		return;
+	}
+
+	FwpmTransactionCommit0((*m_engine).session());
+}
+
+void Transaction::abort()
+{
+	if (m_aborted)
+	{
+		return;
+	}
+
+	auto status = FwpmTransactionAbort0((*m_engine).session());
+
+	if (ERROR_SUCCESS != status)
+	{
+		//
+		// TODO: Specific error message as above
+		//
+		throw std::runtime_error("Could not abort transaction");
+	}
+
+	m_aborted = true;
+}
+
+//static
+bool Transaction::RunReadWrite(std::shared_ptr<FilterEngine> engine, std::function<bool()> operation)
+{
+	return Run(engine, operation, true);
+}
+
+//static
+bool Transaction::RunReadOnly(std::shared_ptr<FilterEngine> engine, std::function<bool()> operation)
+{
+	return Run(engine, operation, false);
+}
+
+//static
+bool Transaction::Run(std::shared_ptr<FilterEngine> engine, std::function<bool()> operation, bool readWrite)
+{
+	Transaction t(engine, readWrite);
+
+	auto status = operation();
+
+	if (!operation())
+	{
+		t.abort();
+	}
+
+	return status;
+}
---
+++ src/libwfp/transaction.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <functional>
+#include <memory>
+#include "FilterEngine.h"
+
+class Transaction
+{
+public:
+
+	Transaction(std::shared_ptr<FilterEngine> engine, bool readWrite);
+	~Transaction();
+	void abort();
+
+	static bool RunReadWrite(std::shared_ptr<FilterEngine> engine, std::function<bool()> operation);
+	static bool RunReadOnly(std::shared_ptr<FilterEngine> engine, std::function<bool()> operation);
+
+private:
+
+	static bool Run(std::shared_ptr<FilterEngine> engine, std::function<bool()> operation, bool readWrite);
+
+	std::shared_ptr<FilterEngine> m_engine;
+	bool m_aborted;
+};
