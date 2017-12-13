243418981fa934837c806ee1a1542132de9eef44
Odd Stranne <odd@mullvad.net>
2017-12-13 14:46:47 +0100

    Add LayerConditions for validating conditions are compatible with a given layer

--- src/tests/tests.cpp
+++ src/tests/tests.cpp
@@ -3,8 +3,6 @@
 
 #include "stdafx.h"
 #include "gtest/gtest.h"
-#include "libcommon/string.h"
-#include <algorithm>
 
 int main(int argc, wchar_t **argv)
 {
@@ -14,87 +12,3 @@ int main(int argc, wchar_t **argv)
 	return status;
 }
 
-TEST(LibCommonString, JoinNothing)
-{
-	ASSERT_EQ(common::string::Join(std::vector<std::wstring>()), L"");
-}
-
-TEST(LibCommonString, JoinSingleElement)
-{
-	ASSERT_EQ(common::string::Join(std::vector<std::wstring>() = { L"foobar" }), L"foobar");
-}
-
-TEST(LibCommonString, JoinMultipleElements)
-{
-	ASSERT_EQ(common::string::Join(std::vector<std::wstring>() =
-		{ L"foobar", L"foobear" }), L"foobar, foobear");
-}
-
-TEST(LibCommonString, FormatFlagsNoMatch)
-{
-	std::vector<std::pair<UINT32, std::wstring> > definitions =
-	{
-		std::make_pair(0x01, L"FLAG_ONE")
-	};
-
-	ASSERT_EQ(common::string::FormatFlags(definitions, (UINT32)0x02), L"[...]");
-}
-
-TEST(LibCommonString, FormatFlagsOneMatchOneFlag)
-{
-	std::vector<std::pair<UINT32, std::wstring> > definitions =
-	{
-		std::make_pair(0x01, L"FLAG_ONE")
-	};
-
-	ASSERT_EQ(common::string::FormatFlags(definitions, (UINT32)0x01), L"FLAG_ONE");
-}
-
-TEST(LibCommonString, FormatFlagsTwoMatchesTwoFlags)
-{
-	std::vector<std::pair<UINT32, std::wstring> > definitions =
-	{
-		std::make_pair(0x01, L"FLAG_ONE"),
-		std::make_pair(0x02, L"FLAG_TWO")
-	};
-
-	ASSERT_EQ(common::string::FormatFlags(definitions, (UINT32)0x03), L"FLAG_ONE, FLAG_TWO");
-}
-
-TEST(LibCommonString, FormatFlagsOneMatchSeveralFlags)
-{
-	std::vector<std::pair<UINT32, std::wstring> > definitions =
-	{
-		std::make_pair(0x01, L"FLAG_ONE")
-	};
-
-	ASSERT_EQ(common::string::FormatFlags(definitions, (UINT32)0x03), L"FLAG_ONE, [...]");
-}
-
-TEST(LibCommonString, FormatFlagsOneMatchSeveralFlagsManyDefinitions)
-{
-	std::vector<std::pair<UINT32, std::wstring> > definitions =
-	{
-		std::make_pair(0x01, L"FLAG_ONE"),
-		std::make_pair(0x04, L"FLAG_THREE"),
-		std::make_pair(0x08, L"FLAG_FOUR")
-	};
-
-	ASSERT_EQ(common::string::FormatFlags(definitions, (UINT32)0x03), L"FLAG_ONE, [...]");
-}
-
-TEST(LibCommonString, FormatIpV4)
-{
-	ASSERT_EQ(common::string::FormatIpV4(0x7f000001), L"127.0.0.1");
-}
-
-TEST(LibCommonString, FormatIpV6)
-{
-	UINT8 ip[] =
-	{
-		0x10, 0x00, 0x20, 0x00, 0x30, 0x00, 0x40, 0x00,
-		0x50, 0x00, 0x60, 0x00, 0x70, 0x00, 0x80, 0x00
-	};
-
-	ASSERT_EQ(common::string::FormatIpV6(ip), L"10:20:30:40:50:60:70:80");
-}
---
+++ src/tests/wfp/layerconditions.cpp
@@ -0,0 +1,33 @@
+#include "stdafx.h"
+#include <initguid.h>
+#include "gtest/gtest.h"
+#include "libwfp/layerconditions.h"
+#include <fwpmu.h>
+#include <guiddef.h>
+
+TEST(LibWfpLayerConditions, IsCompatibleWithCompatibleCondition)
+{
+	ASSERT_TRUE(wfp::LayerConditions::IsCompatible(&FWPM_LAYER_OUTBOUND_TRANSPORT_V6, &FWPM_CONDITION_IP_LOCAL_ADDRESS_TYPE));
+
+	// Match last item in defined condition array, to ensure no off-by-one issues
+	ASSERT_TRUE(wfp::LayerConditions::IsCompatible(&FWPM_LAYER_OUTBOUND_TRANSPORT_V6, &FWPM_CONDITION_CURRENT_PROFILE_ID));
+}
+
+TEST(LibWfpLayerConditions, IsNotCompatibleWithIncompatibleCondition)
+{
+	ASSERT_FALSE(wfp::LayerConditions::IsCompatible(&FWPM_LAYER_OUTBOUND_TRANSPORT_V6, &FWPM_CONDITION_DIRECTION));
+}
+
+DEFINE_GUID(
+	FWPM_LAYER_INVALID,
+	0xa86fd1bf,
+	0x21cd,
+	0x497e,
+	0xa0, 0xbb, 0x17, 0x42, 0x5c, 0x88, 0x5c, 0x58
+);
+
+TEST(LibWfpLayerConditions, IsCompatibleWithInvalidLayerThrows)
+{
+	ASSERT_THROW(wfp::LayerConditions::IsCompatible(&FWPM_LAYER_INVALID, &FWPM_CONDITION_DIRECTION), std::runtime_error);
+}
+
