2d9103b458f1714049ff51472b6bd47a73e44acc
Odd Stranne <odd@mullvad.net>
2017-12-22 22:52:44 +0100

    Add FilterBuilder to simplify filter creation

---
+++ src/libwfp/filterbuilder.cpp
@@ -0,0 +1,96 @@
+#include "stdafx.h"
+#include "filterbuilder.h"
+#include "memory.h"
+
+namespace wfp
+{
+
+FilterBuilder::FilterBuilder()
+{
+	memset(&m_filter, 0, sizeof(m_filter));
+	block();
+}
+
+FilterBuilder &FilterBuilder::name(const std::wstring &name)
+{
+	m_name = name;
+	m_filter.displayData.name = const_cast<wchar_t *>(m_name.c_str());
+
+	return *this;
+}
+
+FilterBuilder &FilterBuilder::description(const std::wstring &description)
+{
+	m_description = description;
+	m_filter.displayData.description = const_cast<wchar_t *>(m_description.c_str());
+
+	return *this;
+}
+
+FilterBuilder &FilterBuilder::persistent()
+{
+	m_filter.flags &= FWPM_FILTER_FLAG_PERSISTENT;
+	return *this;
+}
+
+FilterBuilder &FilterBuilder::boottime()
+{
+	m_filter.flags &= FWPM_FILTER_FLAG_BOOTTIME;
+	return *this;
+}
+
+
+FilterBuilder &FilterBuilder::disabled()
+{
+	m_filter.flags &= FWPM_FILTER_FLAG_DISABLED;
+	return *this;
+}
+
+FilterBuilder &FilterBuilder::provider(const GUID &provider)
+{
+	memcpy(&m_providerKey, &provider, sizeof(m_providerKey));
+	m_filter.providerKey = &m_providerKey;
+
+	return *this;
+}
+
+FilterBuilder &FilterBuilder::layer(const GUID &layer)
+{
+	memcpy(&m_filter.layerKey, &layer, sizeof(m_filter.layerKey));
+	return *this;
+}
+
+FilterBuilder &FilterBuilder::sublayer(const GUID &sublayer)
+{
+	memcpy(&m_filter.subLayerKey, &sublayer, sizeof(m_filter.subLayerKey));
+	return *this;
+}
+
+FilterBuilder &FilterBuilder::weight(UINT64 weight)
+{
+	m_weight = weight;
+
+	m_filter.weight.type = FWP_UINT64;
+	m_filter.weight.uint64 = &m_weight;
+
+	return *this;
+}
+
+FilterBuilder &FilterBuilder::block()
+{
+	m_filter.action.type = FWP_ACTION_BLOCK;
+	return *this;
+}
+
+FilterBuilder &FilterBuilder::permit()
+{
+	m_filter.action.type = FWP_ACTION_PERMIT;
+	return *this;
+}
+
+bool FilterBuilder::build(FilterSink sink)
+{
+	return sink(m_filter);
+}
+
+}
---
+++ src/libwfp/filterbuilder.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <string>
+#include <windows.h>
+#include <fwpmu.h>
+#include <functional>
+
+namespace wfp
+{
+
+//
+// Note: This builder is incomplete!
+//
+// Creating a self-contained object is fairly complex so this is currently not being attempted.
+// Use the builder for creating almost-complete filters and complement them once built.
+//
+// More methods could be added but all fields were not needed at this time.
+//
+
+typedef std::function<bool(FWPM_FILTER0 &)> FilterSink;
+
+class FilterBuilder
+{
+public:
+
+	FilterBuilder();
+
+	FilterBuilder &name(const std::wstring &name);
+	FilterBuilder &description(const std::wstring &description);
+
+	FilterBuilder &persistent();
+	FilterBuilder &boottime();
+	FilterBuilder &disabled();
+
+	FilterBuilder &provider(const GUID &provider);
+	FilterBuilder &layer(const GUID &layer);
+	FilterBuilder &sublayer(const GUID &sublayer);
+
+	FilterBuilder &weight(UINT64 weight);
+
+	FilterBuilder &block();
+	FilterBuilder &permit();
+
+	bool build(FilterSink sink);
+
+private:
+
+	FilterBuilder(const FilterBuilder &);
+	FilterBuilder &operator=(const FilterBuilder &);
+
+	FWPM_FILTER0 m_filter;
+
+	std::wstring m_name;
+	std::wstring m_description;
+	GUID m_providerKey;
+	UINT64 m_weight;
+};
+
+}
