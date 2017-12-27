721538046efb022e41f836e264d34a8c73217d1b
Odd Stranne <odd@mullvad.net>
2017-12-27 16:51:28 +0100

    Correct GUID definition in test code

--- src/tests/wfp/layerconditions.cpp
+++ src/tests/wfp/layerconditions.cpp
@@ -1,5 +1,4 @@
 #include "stdafx.h"
-#include <initguid.h>
 #include "gtest/gtest.h"
 #include "libwfp/layerconditions.h"
 #include <guiddef.h>
@@ -18,16 +17,15 @@ TEST(LibWfpLayerConditions, IsNotCompatibleWithIncompatibleCondition)
 	ASSERT_FALSE(wfp::LayerConditions::IsCompatible(FWPM_LAYER_OUTBOUND_TRANSPORT_V6, FWPM_CONDITION_DIRECTION));
 }
 
-DEFINE_GUID(
-	FWPM_LAYER_INVALID,
-	0xa86fd1bf,
-	0x21cd,
-	0x497e,
-	0xa0, 0xbb, 0x17, 0x42, 0x5c, 0x88, 0x5c, 0x58
-);
-
 TEST(LibWfpLayerConditions, IsCompatibleWithInvalidLayerThrows)
 {
-	ASSERT_THROW(wfp::LayerConditions::IsCompatible(FWPM_LAYER_INVALID, FWPM_CONDITION_DIRECTION), std::runtime_error);
+	static const GUID InvalidLayer = {
+		0xa86fd1bf,
+		0x21cd,
+		0x497e,
+		{ 0xa0, 0xbb, 0x17, 0x42, 0x5c, 0x88, 0x5c, 0x58 }
+	};
+
+	ASSERT_THROW(wfp::LayerConditions::IsCompatible(InvalidLayer, FWPM_CONDITION_DIRECTION), std::runtime_error);
 }
 
