5de9aed16c92344d8b1fd44bada58f080a6f5ec9
Odd Stranne <odd@mullvad.net>
2017-12-28 23:38:10 +0100

    Remove dangerous dtor in Transaction class

--- src/libwfp/transaction.cpp
+++ src/libwfp/transaction.cpp
@@ -7,64 +7,50 @@
 namespace wfp
 {
 
-Transaction::Transaction(std::shared_ptr<FilterEngine> engine, bool readWrite)
-	: m_engine(engine)
-	, m_aborted(false)
+//static
+bool Transaction::Execute(FilterEngine &engine, std::function<bool()> operation)
 {
-	auto status = FwpmTransactionBegin0((*engine).session(), (readWrite ? 0 : FWPM_TXN_READ_ONLY));
+	return Execute(engine, operation, true);
+}
+
+//static
+bool Transaction::ExecuteReadOnly(FilterEngine &engine, std::function<bool()> operation)
+{
+	return Execute(engine, operation, false);
+}
+
+//static
+bool Transaction::Execute(FilterEngine &engine, std::function<bool()> operation, bool readWrite)
+{
+	auto status = FwpmTransactionBegin0(engine.session(), (readWrite ? 0 : FWPM_TXN_READ_ONLY));
 
 	THROW_UNLESS(ERROR_SUCCESS, status, "Initiate WFP transaction");
-}
 
-Transaction::~Transaction()
-{
-	if (m_aborted)
+	bool operationResult = false;
+
+	try
 	{
-		return;
+		operationResult = operation();
+	}
+	catch (...)
+	{
+		// Attemp to abort and throw with inner exception if it fails
+		THROW_UNLESS(ERROR_SUCCESS, FwpmTransactionAbort0(engine.session()), "Abort WFP transaction");
+
+		// Successfully aborted so rethrow original exception
+		throw;
 	}
 
-	FwpmTransactionCommit0((*m_engine).session());
-}
-
-void Transaction::abort()
-{
-	if (m_aborted)
+	if (operationResult)
 	{
-		return;
+		THROW_UNLESS(ERROR_SUCCESS, FwpmTransactionCommit0(engine.session()), "Commit WFP transaction");
+	}
+	else
+	{
+		THROW_UNLESS(ERROR_SUCCESS, FwpmTransactionAbort0(engine.session()), "Abort WFP transaction");
 	}
 
-	auto status = FwpmTransactionAbort0((*m_engine).session());
-
-	THROW_UNLESS(ERROR_SUCCESS, status, "Abort WFP transaction");
-
-	m_aborted = true;
-}
-
-//static
-bool Transaction::RunReadWrite(std::shared_ptr<FilterEngine> engine, std::function<bool()> operation)
-{
-	return Run(engine, operation, true);
-}
-
-//static
-bool Transaction::RunReadOnly(std::shared_ptr<FilterEngine> engine, std::function<bool()> operation)
-{
-	return Run(engine, operation, false);
-}
-
-//static
-bool Transaction::Run(std::shared_ptr<FilterEngine> engine, std::function<bool()> operation, bool readWrite)
-{
-	Transaction t(engine, readWrite);
-
-	auto status = operation();
-
-	if (!operation())
-	{
-		t.abort();
-	}
-
-	return status;
+	return operationResult;
 }
 
 } // namespace wfp
--- src/libwfp/transaction.h
+++ src/libwfp/transaction.h
@@ -11,19 +11,12 @@ class Transaction
 {
 public:
 
-	Transaction(std::shared_ptr<FilterEngine> engine, bool readWrite);
-	~Transaction();
-	void abort();
-
-	static bool RunReadWrite(std::shared_ptr<FilterEngine> engine, std::function<bool()> operation);
-	static bool RunReadOnly(std::shared_ptr<FilterEngine> engine, std::function<bool()> operation);
+	static bool Execute(FilterEngine &engine, std::function<bool()> operation);
+	static bool ExecuteReadOnly(FilterEngine &engine, std::function<bool()> operation);
 
 private:
 
-	static bool Run(std::shared_ptr<FilterEngine> engine, std::function<bool()> operation, bool readWrite);
-
-	std::shared_ptr<FilterEngine> m_engine;
-	bool m_aborted;
+	static bool Execute(FilterEngine &engine, std::function<bool()> operation, bool readWrite);
 };
 
 } // namespace wfp
