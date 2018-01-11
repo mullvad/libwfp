02f37c8906839b2ef688a258047103f2335bad62
Odd Stranne <odd@mullvad.net>
2018-01-11 20:26:52 +0100

    Update ObjectInstaller with capability to add sublayers

--- src/libwfp/objectinstaller.cpp
+++ src/libwfp/objectinstaller.cpp
@@ -31,4 +31,21 @@ bool ObjectInstaller::AddFilter(FilterEngine &engine, FilterBuilder &filterBuild
 	});
 }
 
+//static
+bool ObjectInstaller::AddSublayer(FilterEngine &engine, SublayerBuilder &sublayerBuilder)
+{
+	return sublayerBuilder.build([&](FWPM_SUBLAYER0 &sublayer)
+	{
+		auto status = FwpmSubLayerAdd0(
+			engine.session(),
+			&sublayer,
+			nullptr
+		);
+
+		THROW_UNLESS(ERROR_SUCCESS, status, "Register sublayer with BFE");
+
+		return true;
+	});
+}
+
 }
--- src/libwfp/objectinstaller.h
+++ src/libwfp/objectinstaller.h
@@ -3,6 +3,7 @@
 #include "conditionbuilder.h"
 #include "filterbuilder.h"
 #include "filterengine.h"
+#include "sublayerbuilder.h"
 
 namespace wfp
 {
@@ -12,6 +13,7 @@ class ObjectInstaller
 public:
 
 	static bool AddFilter(FilterEngine &engine, FilterBuilder &filterBuilder, ConditionBuilder &conditionBuilder);
+	static bool AddSublayer(FilterEngine &engine, SublayerBuilder &sublayerBuilder);
 };
 
 }
