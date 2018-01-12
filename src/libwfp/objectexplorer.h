6179bde796b14222f1bcc198f0ab900dd8ff3787
Odd Stranne <odd@mullvad.net>
2018-01-12 11:39:37 +0100

    Add ObjectDeleter

--- src/libwfp/objectexplorer.h
+++ src/libwfp/objectexplorer.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include "filterengine.h"
+#include <guiddef.h>
+#include <fwpmu.h>
+#include <functional>
+
+namespace wfp
+{
+
+class ObjectExplorer
+{
+public:
+
+	static bool GetFilter(FilterEngine &engine, const GUID &key, std::function<bool(const FWPM_FILTER0 &)> callback);
+	static bool GetProvider(FilterEngine &engine, const GUID &key, std::function<bool(const FWPM_PROVIDER0 &)> callback);
+	static bool GetSublayer(FilterEngine &engine, const GUID &key, std::function<bool(const FWPM_SUBLAYER0 &)> callback);
+};
+
+}
