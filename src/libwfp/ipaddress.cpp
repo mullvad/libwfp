9eac2c77602061635a5195b8c43069886c14021b
Odd Stranne <odd@mullvad.net>
2018-03-12 15:24:47 +0100

    Rename IpAddr class into IpAddress and complete implementation

---
+++ src/libwfp/ipaddress.cpp
@@ -0,0 +1,134 @@
+#include "stdafx.h"
+#include "ipaddress.h"
+#include "libcommon/string.h"
+#include <ws2tcpip.h>
+#include <winsock2.h>
+#include <stdexcept>
+
+namespace
+{
+
+uint32_t PackIpv4(uint8_t a, uint8_t b, uint8_t c, uint8_t d)
+{
+	return ((uint32_t(a) << 24) | (uint32_t(b) << 16) | (uint32_t(c) << 8) | d);
+}
+
+uint32_t Ipv4FromInitializerList(const std::initializer_list<uint8_t> &netnode)
+{
+	auto it = netnode.begin();
+
+	auto a = *it++;
+	auto b = *it++;
+	auto c = *it++;
+	auto d = *it++;
+
+	return PackIpv4(a, b, c, d);
+}
+
+} // anonymous namespace
+
+namespace wfp
+{
+
+IpAddress::Literal::Literal(const std::initializer_list<uint8_t> &netnode)
+{
+	if (netnode.size() != 4)
+	{
+		throw std::runtime_error("Invalid IPv4 specification");
+	}
+
+	address = Ipv4FromInitializerList(netnode);
+}
+
+IpAddress::Literal6::Literal6(const std::initializer_list<uint16_t> &netnode)
+{
+	if (netnode.size() != 8)
+	{
+		throw std::runtime_error("Invalid IPv6 specification");
+	}
+
+	size_t index = 0;
+
+	for (uint16_t hextet : netnode)
+	{
+		address[index++] = uint8_t((hextet & 0xFF00) >> 8);
+		address[index++] = uint8_t(hextet & 0xFF);
+	}
+}
+
+IpAddress::IpAddress(const std::wstring &netnode)
+{
+	uint8_t buffer[sizeof(IN6_ADDR)];
+
+	if (1 == InetPtonW(AF_INET, netnode.c_str(), &buffer))
+	{
+		auto parsed = reinterpret_cast<const IN_ADDR *>(&buffer);
+
+		m_ipv4 = ntohl(parsed->s_addr);
+		m_type = Type::Ipv4;
+
+		memset(m_ipv6, 0, sizeof(m_ipv6));
+	}
+	else if (1 == InetPtonW(AF_INET6, netnode.c_str(), &buffer))
+	{
+		auto parsed = reinterpret_cast<const IN6_ADDR *>(&buffer);
+
+		memcpy(m_ipv6, parsed->u.Byte, sizeof(m_ipv6));
+		m_type = Type::Ipv6;
+
+		m_ipv4 = 0;
+	}
+	else
+	{
+		throw std::runtime_error("Parser failure: Invalid IP address.");
+	}
+}
+
+IpAddress::IpAddress(uint32_t netnode)
+	: m_ipv4(netnode)
+	, m_type(Type::Ipv4)
+{
+	memset(m_ipv6, 0, sizeof(m_ipv6));
+}
+
+IpAddress::IpAddress(const IpAddress::Literal &netnode)
+	: m_ipv4(netnode.address)
+	, m_type(Type::Ipv4)
+{
+	memset(m_ipv6, 0, sizeof(m_ipv6));
+}
+
+IpAddress::IpAddress(const FWP_BYTE_ARRAY16 &netnode)
+	: m_ipv4(0)
+	, m_type(Type::Ipv6)
+{
+	memcpy(m_ipv6, netnode.byteArray16, sizeof(m_ipv6));
+}
+
+IpAddress::IpAddress(const IpAddress::Literal6 &netnode)
+	: m_ipv4(0)
+	, m_type(Type::Ipv6)
+{
+	memcpy(m_ipv6, netnode.address, sizeof(m_ipv6));
+}
+
+std::wstring IpAddress::toString() const
+{
+	switch (m_type)
+	{
+		case Type::Ipv4:
+		{
+			return common::string::FormatIpv4(m_ipv4);
+		}
+		case Type::Ipv6:
+		{
+			return common::string::FormatIpv6(m_ipv6);
+		}
+		default:
+		{
+			throw std::logic_error("Missing case handler in switch clause");
+		}
+	}
+}
+
+}
---
+++ src/libwfp/ipaddress.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <windows.h>
+#include <fwptypes.h>
+#include <initializer_list>
+#include <string>
+
+namespace wfp
+{
+
+//
+// Abstraction over a specific IPv4/IPv6 address.
+// For IPv4, host byte order is used.
+//
+class IpAddress
+{
+public:
+
+	struct Literal;
+	struct Literal6;
+
+	// Generic
+	IpAddress(const std::wstring &netnode);
+
+	// IPv4
+	IpAddress(uint32_t netnode);
+	IpAddress(const Literal &netnode);
+
+	// IPv6
+	IpAddress(const FWP_BYTE_ARRAY16 &netnode);
+	IpAddress(const Literal6 &netnode);
+
+	//
+	// Helper-types to construct from literal address
+	//
+
+	struct Literal
+	{
+		Literal(const std::initializer_list<uint8_t> &netnode);
+		uint32_t address;
+	};
+
+	struct Literal6
+	{
+		Literal6(const std::initializer_list<uint16_t> &netnode);
+		uint8_t address[16];
+	};
+
+	enum Type
+	{
+		Ipv4,
+		Ipv6
+	};
+
+	Type type() const
+	{
+		return m_type;
+	}
+
+	uint32_t addr() const
+	{
+		return m_ipv4;
+	}
+
+	const FWP_BYTE_ARRAY16 &addr6() const
+	{
+		return *reinterpret_cast<const FWP_BYTE_ARRAY16 *>(m_ipv6);
+	}
+
+	std::wstring toString() const;
+
+private:
+
+	uint32_t m_ipv4;
+	uint8_t m_ipv6[16];
+
+	Type m_type;
+};
+
+}
