b8fc9ff3f6d8e5aa92916573dc2df4352e4cfd66
Odd Stranne <odd@mullvad.net>
2018-03-12 22:10:53 +0100

    Add firewall rules

---
+++ src/wfpctl/rules/permitlan.cpp
@@ -0,0 +1,97 @@
+#include "stdafx.h"
+#include "permitlan.h"
+#include "wfpctl/mullvadguids.h"
+#include "libwfp/filterbuilder.h"
+#include "libwfp/conditionbuilder.h"
+#include "libwfp/ipaddress.h"
+#include "libwfp/conditions/conditionlocalip.h"
+#include "libwfp/conditions/conditionremoteip.h"
+
+using namespace wfp::conditions;
+
+namespace rules
+{
+
+bool PermitLan::apply(IObjectInstaller &objectInstaller)
+{
+	wfp::FilterBuilder filterBuilder;
+
+	//
+	// #1 locally-initiated on 10/8
+	//
+
+	filterBuilder
+		.key(MullvadGuids::FilterPermitLan_10_8())
+		.name(L"Permit locally-initiated traffic on 10/8")
+		.description(L"This filter is part of a rule that permits LAN traffic")
+		.provider(MullvadGuids::Provider())
+		.layer(FWPM_LAYER_ALE_AUTH_CONNECT_V4)
+		.sublayer(MullvadGuids::SublayerWhitelist())
+		.weight(wfp::FilterBuilder::WeightClass::Max)
+		.permit();
+
+	wfp::ConditionBuilder conditionBuilder(FWPM_LAYER_ALE_AUTH_CONNECT_V4);
+
+	conditionBuilder.add_condition(new ConditionLocalIp(wfp::IpAddress::Literal({ 10, 0, 0, 0 }), 8));
+	conditionBuilder.add_condition(new ConditionRemoteIp(wfp::IpAddress::Literal({ 10, 0, 0, 0 }), 8));
+
+	if (!objectInstaller.addFilter(filterBuilder, conditionBuilder))
+	{
+		return false;
+	}
+
+	//
+	// #2 locally-initiated on 172.16/12
+	//
+
+	filterBuilder
+		.key(MullvadGuids::FilterPermitLan_172_16_12())
+		.name(L"Permit locally-initiated traffic on 172.16/12");
+
+	conditionBuilder.reset();
+
+	conditionBuilder.add_condition(new ConditionLocalIp(wfp::IpAddress::Literal({ 172, 16, 0, 0 }), 12));
+	conditionBuilder.add_condition(new ConditionRemoteIp(wfp::IpAddress::Literal({ 172, 16, 0, 0 }), 12));
+
+	if (!objectInstaller.addFilter(filterBuilder, conditionBuilder))
+	{
+		return false;
+	}
+
+	//
+	// #3 locally-initiated on 192.168/16
+	//
+
+	filterBuilder
+		.key(MullvadGuids::FilterPermitLan_192_168_16())
+		.name(L"Permit locally-initiated traffic on 192.168/16");
+
+	conditionBuilder.reset();
+
+	conditionBuilder.add_condition(new ConditionLocalIp(wfp::IpAddress::Literal({ 192, 168, 0, 0 }), 16));
+	conditionBuilder.add_condition(new ConditionRemoteIp(wfp::IpAddress::Literal({ 192, 168, 0, 0 }), 16));
+
+	if (!objectInstaller.addFilter(filterBuilder, conditionBuilder))
+	{
+		return false;
+	}
+
+	//
+	// #4 LAN to multicast
+	//
+
+	filterBuilder
+		.key(MullvadGuids::FilterPermitLan_Multicast())
+		.name(L"Permit locally-initiated multicast traffic");
+
+	conditionBuilder.reset();
+
+	conditionBuilder.add_condition(new ConditionLocalIp(wfp::IpAddress::Literal({ 10, 0, 0, 0 }), 8));
+	conditionBuilder.add_condition(new ConditionLocalIp(wfp::IpAddress::Literal({ 172, 16, 0, 0 }), 12));
+	conditionBuilder.add_condition(new ConditionLocalIp(wfp::IpAddress::Literal({ 192, 168, 0, 0 }), 16));
+	conditionBuilder.add_condition(new ConditionRemoteIp(wfp::IpAddress::Literal({ 224, 0, 0, 0 }), 24));
+
+	return objectInstaller.addFilter(filterBuilder, conditionBuilder);
+}
+
+}
