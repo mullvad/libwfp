29ca7d085cea623703791a7d697fe16cae834642
David Lönnhager <david.l@mullvad.net>
2020-01-27 13:18:00 +0100

    Use VS testing fw instead of Google Test

--- src/test-libwfp/wfp/ipaddress.cpp
+++ src/test-libwfp/wfp/ipaddress.cpp
@@ -1,45 +1,76 @@
 #include "stdafx.h"
-#include "gtest/gtest.h"
+#include "CppUnitTest.h"
 #include "libwfp/ipaddress.h"
 
 using IpAddress = ::wfp::IpAddress;
 
-TEST(LibWfpIpAddr, ConstructIpv4)
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Microsoft::VisualStudio::CppUnitTestFramework
 {
-	static const wchar_t *literalString = L"127.0.0.1";
 
-	IpAddress ip_from_literal(IpAddress::Literal({ 127, 0, 0, 1 }));
-	IpAddress ip_from_string(literalString);
-	IpAddress ip_from_raw(0x7f000001);
-
-	ASSERT_TRUE(ip_from_literal.type() == IpAddress::Type::Ipv4);
-	ASSERT_TRUE(ip_from_string.type() == IpAddress::Type::Ipv4);
-	ASSERT_TRUE(ip_from_raw.type() == IpAddress::Type::Ipv4);
-
-	ASSERT_STREQ(ip_from_string.toString().c_str(), literalString);
-	ASSERT_STREQ(ip_from_literal.toString().c_str(), literalString);
-	ASSERT_STREQ(ip_from_raw.toString().c_str(), literalString);
+template<>
+std::wstring ToString<wfp::IpAddress::Type>(const wfp::IpAddress::Type &address)
+{
+	switch (address)
+	{
+		case IpAddress::Type::Ipv4:
+			return L"IpAddress::Type::Ipv4";
+		case IpAddress::Type::Ipv6:
+			return L"IpAddress::Type::Ipv6";
+	}
+	return L"IpAddress::Type::UNKNOWN";
 }
 
-TEST(LibWfpIpAddr, ConstructIpv6)
+}
+
+namespace testlibwfp
 {
-	static const wchar_t *literalString = L"1000:1100:1200:1300:1400:1500:1600:1700";
 
-	IpAddress ip_from_literal(IpAddress::Literal6({ 0x1000, 0x1100, 0x1200, 0x1300, 0x1400, 0x1500, 0x1600, 0x1700 }));
-	IpAddress ip_from_string(literalString);
+TEST_CLASS(TestLibWfpIpAddr)
+{
+public:
 
-	const uint8_t raw[16] = { 0x10, 00, 0x11, 00, 0x12, 00, 0x13, 00, 0x14, 00, 0x15, 00, 0x16, 00, 0x17, 00 };
+	TEST_METHOD(ConstructIpv4)
+	{
+		static const wchar_t *literalString = L"127.0.0.1";
 
-	FWP_BYTE_ARRAY16 netnode;
-	memcpy(netnode.byteArray16, raw, sizeof(netnode.byteArray16));
+		IpAddress ip_from_literal(IpAddress::Literal({ 127, 0, 0, 1 }));
+		IpAddress ip_from_string(literalString);
+		IpAddress ip_from_raw(0x7f000001);
 
-	IpAddress ip_from_raw(netnode);
+		Assert::AreEqual(ip_from_literal.type(), IpAddress::Type::Ipv4);
+		Assert::AreEqual(ip_from_string.type(), IpAddress::Type::Ipv4);
+		Assert::AreEqual(ip_from_raw.type(), IpAddress::Type::Ipv4);
 
-	ASSERT_TRUE(ip_from_literal.type() == IpAddress::Type::Ipv6);
-	ASSERT_TRUE(ip_from_string.type() == IpAddress::Type::Ipv6);
-	ASSERT_TRUE(ip_from_raw.type() == IpAddress::Type::Ipv6);
+		Assert::AreEqual(ip_from_string.toString().c_str(), literalString);
+		Assert::AreEqual(ip_from_literal.toString().c_str(), literalString);
+		Assert::AreEqual(ip_from_raw.toString().c_str(), literalString);
+	}
 
-	ASSERT_STREQ(ip_from_string.toString().c_str(), literalString);
-	ASSERT_STREQ(ip_from_literal.toString().c_str(), literalString);
-	ASSERT_STREQ(ip_from_raw.toString().c_str(), literalString);
+	TEST_METHOD(ConstructIpv6)
+	{
+		static const wchar_t *literalString = L"1000:1100:1200:1300:1400:1500:1600:1700";
+
+		IpAddress ip_from_literal(IpAddress::Literal6({ 0x1000, 0x1100, 0x1200, 0x1300, 0x1400, 0x1500, 0x1600, 0x1700 }));
+		IpAddress ip_from_string(literalString);
+
+		const uint8_t raw[16] = { 0x10, 00, 0x11, 00, 0x12, 00, 0x13, 00, 0x14, 00, 0x15, 00, 0x16, 00, 0x17, 00 };
+
+		FWP_BYTE_ARRAY16 netnode;
+		memcpy(netnode.byteArray16, raw, sizeof(netnode.byteArray16));
+
+		IpAddress ip_from_raw(netnode);
+
+		Assert::AreEqual(ip_from_literal.type(), IpAddress::Type::Ipv6);
+		Assert::AreEqual(ip_from_string.type(), IpAddress::Type::Ipv6);
+		Assert::AreEqual(ip_from_raw.type(), IpAddress::Type::Ipv6);
+
+		Assert::AreEqual(ip_from_string.toString().c_str(), literalString);
+		Assert::AreEqual(ip_from_literal.toString().c_str(), literalString);
+		Assert::AreEqual(ip_from_raw.toString().c_str(), literalString);
+	}
+
+};
+
 }
--- src/test-libwfp/wfp/layerconditions.cpp
+++ src/test-libwfp/wfp/layerconditions.cpp
@@ -1,31 +1,43 @@
 #include "stdafx.h"
-#include "gtest/gtest.h"
+#include "CppUnitTest.h"
 #include "libwfp/layerconditions.h"
 #include <guiddef.h>
 #include <fwpmu.h>
 
-TEST(LibWfpLayerConditions, IsCompatibleWithCompatibleCondition)
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace testlibwfp
 {
-	ASSERT_TRUE(wfp::LayerConditions::IsCompatible(FWPM_LAYER_OUTBOUND_TRANSPORT_V6, FWPM_CONDITION_IP_LOCAL_ADDRESS_TYPE));
 
-	// Match last item in defined condition array, to ensure no off-by-one issues
-	ASSERT_TRUE(wfp::LayerConditions::IsCompatible(FWPM_LAYER_OUTBOUND_TRANSPORT_V6, FWPM_CONDITION_CURRENT_PROFILE_ID));
-}
-
-TEST(LibWfpLayerConditions, IsNotCompatibleWithIncompatibleCondition)
+TEST_CLASS(TestLibWfpLayerConditions)
 {
-	ASSERT_FALSE(wfp::LayerConditions::IsCompatible(FWPM_LAYER_OUTBOUND_TRANSPORT_V6, FWPM_CONDITION_DIRECTION));
+public:
+
+	TEST_METHOD(IsCompatibleWithCompatibleCondition)
+	{
+		Assert::IsTrue(wfp::LayerConditions::IsCompatible(FWPM_LAYER_OUTBOUND_TRANSPORT_V6, FWPM_CONDITION_IP_LOCAL_ADDRESS_TYPE));
+
+		// Match last item in defined condition array, to ensure no off-by-one issues
+		Assert::IsTrue(wfp::LayerConditions::IsCompatible(FWPM_LAYER_OUTBOUND_TRANSPORT_V6, FWPM_CONDITION_CURRENT_PROFILE_ID));
+	}
+
+	TEST_METHOD(IsNotCompatibleWithIncompatibleCondition)
+	{
+		Assert::IsFalse(wfp::LayerConditions::IsCompatible(FWPM_LAYER_OUTBOUND_TRANSPORT_V6, FWPM_CONDITION_DIRECTION));
+	}
+
+	TEST_METHOD(IsCompatibleWithInvalidLayerThrows)
+	{
+		static const GUID InvalidLayer = {
+			0xa86fd1bf,
+			0x21cd,
+			0x497e,
+			{ 0xa0, 0xbb, 0x17, 0x42, 0x5c, 0x88, 0x5c, 0x58 }
+		};
+
+		Assert::ExpectException<std::runtime_error>([&]() { wfp::LayerConditions::IsCompatible(InvalidLayer, FWPM_CONDITION_DIRECTION); });
+	}
+
+};
+
 }
-
-TEST(LibWfpLayerConditions, IsCompatibleWithInvalidLayerThrows)
-{
-	static const GUID InvalidLayer = {
-		0xa86fd1bf,
-		0x21cd,
-		0x497e,
-		{ 0xa0, 0xbb, 0x17, 0x42, 0x5c, 0x88, 0x5c, 0x58 }
-	};
-
-	ASSERT_THROW(wfp::LayerConditions::IsCompatible(InvalidLayer, FWPM_CONDITION_DIRECTION), std::runtime_error);
-}
-
