e0c7d5b4a944d85a2165daf4fce8b9dcfe140421
Sebastian Holmin <sebastian.holmin@mullvad.net>
2024-09-05 10:39:27 +0200

    Fix includes

--- src/libwfp/conditions/conditionl2flags.cpp
+++ src/libwfp/conditions/conditionl2flags.cpp
@@ -1,8 +1,6 @@
-#pragma once
-
+#include "stdafx.h"
 #include <sstream>
 #include "conditionl2flags.h"
-#include "stdafx.h"
 
 namespace wfp::conditions {
 
--- src/libwfp/conditions/conditionl2flags.h
+++ src/libwfp/conditions/conditionl2flags.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "comparison.h"
 #include "ifiltercondition.h"
 
 namespace wfp::conditions {
